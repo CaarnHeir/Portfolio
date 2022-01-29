65c9a7a8621692b895c76e052c575eb881c6d128
Caarn Heir <caarnheir@gmail.com>
2022-01-28 20:28:53 -0500

    added hamburger and close icons to navbar

--- client/components/MenuToggle.js
+++ client/components/MenuToggle.js
@@ -1,11 +1,12 @@
 // import React from 'react';
-import { Box } from "@chakra-ui/react"
+import { Box, IconButton } from "@chakra-ui/react"
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons'
 // import { CloseIcon, MenuIcon } from 'react-icons'
 
 const MenuToggle = ({ toggle, isOpen }) => {
   return (
     <Box display={{ base: "block", md: "none" }} onClick={toggle}>
-      {isOpen ? <img src='/sun.svg'/> : <img src='/sun.svg'/>}
+      {isOpen ?  <IconButton icon= {<CloseIcon/>}/>: <IconButton icon= {<HamburgerIcon/>}/>}
     </Box>
   )
 };
