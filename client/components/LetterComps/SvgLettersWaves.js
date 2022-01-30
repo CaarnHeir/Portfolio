20cd2a6f3823096b05a39cfc03c4ce5df713703e
Caarn Heir <caarnheir@gmail.com>
2022-01-29 21:56:21 -0500

    added stylesheet for svgletters componenets, created transition for svg text for mobile

--- client/components/LetterComps/SvgLettersWaves.js
+++ client/components/LetterComps/SvgLettersWaves.js
@@ -3,8 +3,8 @@ import styles from '../../styles/SvgLetters.module.css'
 
 const SvgLettersWaves = () => {
   return (
-      <svg height='100%' width='100%' fontFamily='Montserrat'>
-          <text x='50%' y='50%' fill='black'>CH</text>
+      <svg className={styles.svg} height='175%' width='100%' fontFamily='Montserrat' fontSize='35em' fontWeight='bold' text-transform='uppercase'>
+          <text x='50%' y='50%' fill='black' textAnchor='middle'>CH</text>
       </svg>
   )
 };
