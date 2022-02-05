38b8906ad5a71da8bd4ad941949c718c6354cf78
Caarn Heir <caarnheir@gmail.com>
2022-02-04 23:29:24 -0500

    fixed svg spill over on letters components

--- client/components/LetterComps/SvgLettersFire.js
+++ client/components/LetterComps/SvgLettersFire.js
@@ -4,17 +4,17 @@ import styles from '../../styles/SvgLettersFire.module.css'
 
 const SvgLettersFire = () => {
   return (
-    <div>
-        <Flex justifyContent= 'center' height='500px' position='static'>
-        <video className={styles.video} autoPlay playsInline muted loop preload='true'><source src='/Fire.mp4' type='video/mp4'/></video>
-            <svg className={styles.svg} height='50%' width='100%' fontFamily='Montserrat' fontSize='35em' fontWeight='bold' text-transform='uppercase'>
+    <div display='relative'>
+        <Flex justifyContent= 'center' height='500px'  width='100%' >
+            <video className={styles.video} autoPlay playsInline muted loop preload='true'><source src='/Fire.mp4' type='video/mp4'/></video>
+            <svg className={styles.svg} viewBox='0 0 889 500' fontFamily='Montserrat' fontSize='35em' fontWeight='bold' text-transform='uppercase'>
                 <defs>
                     <mask id='mask' x='0' y='0' height='100%' width='100%'>
-                        <rect className={styles.rect} x='0px' y='0px' height='80%' width='100%'/>
-                        <text  x='50%' y='25%' fill='black' textAnchor='middle'>CH</text>
+                        <rect className={styles.rect} height='100%' width='100%'/>
+                        <text  x='50%' y='85%' fill='black' textAnchor='middle'>CH</text>
                     </mask>
                 </defs>
-                <rect x='0' y='0' height='500px' width='100%'/>
+                <rect x='0' y='0' height='100%' width='100%'/>
             </svg>
         </Flex>
       </div>
--- client/components/LetterComps/SvgLettersWaves.js
+++ client/components/LetterComps/SvgLettersWaves.js
@@ -4,20 +4,20 @@ import { Flex } from '@chakra-ui/react'
 
 const SvgLettersWaves = () => {
   return (
-      <div>
-        <Flex justifyContent= 'center' height='500px'>
+    <div display='relative'>
+    <Flex justifyContent= 'center' height='500px'  width='100%' >
         <video className={styles.video} autoPlay playsInline muted loop preload='true'><source src='/Waves.mp4' type='video/mp4'/></video>
-            <svg className={styles.svg} height='50%' width='100%' fontFamily='Montserrat' fontSize='35em' fontWeight='bold' text-transform='uppercase' >
-                <defs>
-                    <mask id='mask' x='0' y='0' height='100%' width='100%'>
-                        <rect className={styles.rect} x='0px' y='0px' height='80%' width='100%'/>
-                        <text  x='50%' y='25%' fill='black' textAnchor='middle'>CH</text>
-                    </mask>
-                </defs>
-                <rect x='0' y='0' height='500px' width='100%'/>
-            </svg>
-        </Flex>
-      </div>
+        <svg className={styles.svg} viewBox='0 0 889 500' fontFamily='Montserrat' fontSize='35em' fontWeight='bold' text-transform='uppercase'>
+            <defs>
+                <mask id='mask' x='0' y='0' height='100%' width='100%'>
+                    <rect className={styles.rect} height='100%' width='100%'/>
+                    <text  x='50%' y='85%' fill='black' textAnchor='middle'>CH</text>
+                </mask>
+            </defs>
+            <rect x='0' y='0' height='100%' width='100%'/>
+        </svg>
+    </Flex>
+  </div>
   )
 };
 
