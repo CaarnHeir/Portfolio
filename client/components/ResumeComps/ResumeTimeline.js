2aaa2591e3799e589fcf7c2ac255f6f2f3baef09
Caarn Heir <caarnheir@gmail.com>
2022-02-05 21:42:49 -0500

    Built the timeline component, need to populate with actual copy

--- client/components/ResumeComps/ResumeTimeline.js
+++ client/components/ResumeComps/ResumeTimeline.js
@@ -1,10 +1,57 @@
 import { Center } from '@chakra-ui/react';
-import React from 'react';
+
+import styles from '../../styles/ResumeTimeline.module.css'
 
 const ResumeTimeline = () => {
   return (
-      <Center h='1000px'>
-
+      <Center  marginBottom='100px'>
+          <ul className={styles.timeline}>
+            <li>
+                <div className={styles.directionr}>
+                    <div className={styles.flagwrapper}>
+                        <span className={styles.flag}>Current title</span>
+                        <span className={styles.timewrapper}><span className={styles.time}>2013 - present</span></span>
+                    </div>
+                    <div className={styles.desc}>aasdfasdfasdffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffe!</div>
+                </div>
+            </li>
+            <li>
+                <div className={styles.directionl}>
+                    <div className={styles.flagwrapper}>
+                        <span className={styles.flag}>Company</span>
+                        <span className={styles.timewrapper}><span className={styles.time}>2011 - 2013</span></span>
+                    </div>
+                    <div className={styles.desc}>gggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggggg.</div>
+                </div>
+            </li>
+            <li>
+                <div className={styles.directionr}>
+                    <div className={styles.flagwrapper}>
+                        <span className={styles.flag}>University</span>
+                        <span className={styles.timewrapper}><span className={styles.time}>2008 - 2011</span></span>
+                    </div>
+                    <div className={styles.desc}>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</div>
+                </div>
+            </li>
+            <li>
+                <div className={styles.directionl}>
+                    <div className={styles.flagwrapper}>
+                        <span className={styles.flag}>Company</span>
+                        <span className={styles.timewrapper}><span className={styles.time}>2011 - 2013</span></span>
+                    </div>
+                    <div className={styles.desc}>ggghdfghdfghdfghdfghdfghggggggggggggggggggsdfgsdfgsdfhsdfhsfdhsdfhsfdhgggggggg.</div>
+                </div>
+            </li>
+            <li>
+                <div className={styles.directionr}>
+                    <div className={styles.flagwrapper}>
+                        <span className={styles.flag}>University</span>
+                        <span className={styles.timewrapper}><span className={styles.time}>2008 - 2011</span></span>
+                    </div>
+                    <div className={styles.desc}>hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh</div>
+                </div>
+            </li>
+            </ul>
       </Center>
   )
 };
